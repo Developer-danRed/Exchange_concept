a593f22a945a7e5ed4ef22145771db45ee6d0cee
S.PK <102419150+develper-Selvam-PK@users.noreply.github.com>
2022-05-25 07:55:06 -0700

    Update cloudnary.js

--- cloudnary.js
+++ cloudnary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary')
 cloudinary.config({
-    cloud_name: "dzomvuuer",
-    api_key: "262569576449585",
-    api_secret:"RFiYfmtMSjB6Ns6JyyYFR7qYFwA"
+    cloud_name: "Colud_Name",
+    api_key: "Api_Key",
+    api_secret:"Api_Secret"
 })
 module.exports = cloudinary
